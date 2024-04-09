8c78840c1a66797078ce9dec6f0baa35c6e6ca3f
Sarana <sarana09@mail.ru>
2024-04-09 13:45:38 +0700

    Новая программа с аргументами

--- cmdargs.c
+++ cmdargs.c
@@ -2,17 +2,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-int main(int argc, char *argv[], char *envp[]) {	
+int main(int argc, char *argv[], char *envp[])
+ {	
 	int i;
  	printf("Кол-во аргументов: %d\n\n", argc);
-	for (int i=0; i<argc; i++){
-		printf("аргумент %d: %s\n", i, argv[i]);
-  	}
-	printf("\n");
-	i=0;
-	while (envp[i]!=NULL){
-		printf ("%s\n", envp[i]);
-		i++;
+	if (argc<1){
+		printf("Ошибка\n");
+		exit(-1); 
+	} 
+		else if (argc==1){
+			while (envp[i]!=NULL){
+				printf ("%s\n", envp[i]);
+				i++;
 	}
+}
 return 0;
 }
