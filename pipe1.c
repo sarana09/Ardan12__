c987a7308ee9905d005f747916b63b64d2c8aa8c
Sarana <sarana09@mail.ru>
2024-04-30 14:37:52 +0700

    Добавили второй pipe для передачи информации от ребенка к родителю

--- pipe1.c
+++ pipe1.c
@@ -4,70 +4,100 @@
 #include <stdlib.h>
 int main(){
 /*Создаем массив для хранения файловых дескриптов для чтения и для записи*/
-	int fd[2];
+	int fd1[2], fd2[2];
 	pid_t result;
 	size_t size;
-	char string[] = "Hello, world!";
-	char resstring[14];
+	char string1[] = "Hello, child!", string2[] = "Hello, parent!";
+	char resstring1[13], resstring2[14];
 /*Попытаемся создать pipe*/
-	if(pipe(fd)<0){
+	if(pipe(fd1) < 0){
 /*Если создать pipe не удалось, печатаем об этом сообщение и прекращаем работу.*/
-		printf("Can\'t create pipe\n");
+		printf("Can't create pipe\n");
 		exit(-1);
-		}
+	}
 	result = fork();
+	if(pipe(fd2) < 0){
+                printf("Can't create pipe\n");
+                exit(-1);
+        }
+        result = fork();
 	if (result<0){
 		printf("Ошибка выолнения fork()\n");
 		exit(-1);
-}
+        }
 	else if (result>0){
-		if (close(fd[0])<0){
-		printf("Ошибка при закрытии pipe на чтение\n ");
-	exit(-1);
-	}
-/*Пробуем записать в pipe 14байт из нашего массива, т.е. всю строку "hello world!" вместе с признаком конца строки */	
-		size = write(fd[1], string, 14);
-	if(size !=14){
+		if (close(fd1[0])<0){
+			printf("Ошибка при закрытии pipe на чтение\n ");
+	 		exit(-1);
+		}
+		if (close(fd2[1])<0){
+                	printf("Ошибка при закрытии pipe на запись\n ");
+       			 exit(-1);
+		}
+		/*Пробуем записать в pipe 14байт из нашего массива, т.е. всю строку "hello world!" вместе с признаком конца строки */	
+		size = write(fd1[1], string1, 13);
+		if(size !=13){
 /*Если  записалось меньшее кол-во байт, сообщаем об ошибке*/
-		 printf("Can\'t write all string\n");
-		 exit(-1);
+			 printf("Can't write all string\n");
+		 	 exit(-1);
 		}
-		printf("Процесс - родитель записал информацию в pipe и заканчивает работу\n");
-		if (close(fd[1]) < 0){
-                printf("Can\'t close output stream\n");
-                exit(-1);
-        }
-}
-else {
-		if (close(fd[1])<0){
-                printf("Can\'t close output stream\n");
-                exit(-1);
-        }
- 
-	/*Пробуем прочитать из  pipe 14 байт в другой массив т.е всю записан-ю строку*/
-		size = read(fd[0], resstring, 14);
-//Проверка кол-ва байт, прочитанныхь изь pipe () c помощью read()
-	if(size!=14) {
+		printf("Процесс - родитель записал информацию в pipe\n");
+		size = read(fd2[0], resstring2, 14);
+		//Проверка кол-ва байт, прочитанныхь изь pipe () c помощью read()
+        	if(size!=14) {
 /*Если прочитать не смогли . сообщаем об ошибке*/
-		printf("Can\'t read string\n");
-		exit(-1);
-		}
-/*Печатаем прочитанную строку*/
-		printf("%s\n", resstring);
-		exit (-1);
-//Закрытие pipe для чтения
-	if(close(fd[0])<0){
-		printf("Can\'t close input stream\n");
-		 exit(-1);
-	}
-// Закрытие pipe для записи
-	if (close(fd[1])<0){
-	  	printf("Can\'t close output stream\n");
-		exit(-1);
-	
+                	printf("Can't read string\n");
+                	exit(-1);
+                }
+		printf("Информация от родительского процесса: %s\n", resstring2);
+		if (close(fd1[1]) < 0){
+                	printf("Can't close output stream\n");
+                	exit(-1);
+        	}
+		if (close(fd2[0]) < 0){
+        	        printf("Can't close output stream\n");
+                	exit(-1);
 	}
 }
-	return 0;
+else {          
+	//закрытие pipe  для записи
+	if (close(fd1[1])<0){
+                printf("Can't close output stream\n");
+                exit(-1);
+        }
+        if (close(fd2[0])<0){
+                printf("Can't close output stream\n");
+                exit(-1);
+        }
 
+	/*Пробуем прочитать из  pipe 14 байт в другой массив т.е всю записан-ю строку*/
+	size = read(fd1[0], resstring1, 13);
+	if(size!=13) {
+	/*Если прочитать не смогли . сообщаем об ошибке*/
+		printf("Can't read string\n");
+		exit(-1);
+		}
+       	printf("%s\n", resstring1);
+	 size = write(fd2[1], string2, 14);
+        if(size !=14){
+/*Если  записалось меньшее кол-во байт, сообщаем об ошибке*/
+                 printf("Can't write all string\n");
+                 exit(-1);
+                }
+	printf("Процесс - ребенок записал информацию в pipe\n");
+//Закрытие pipe для чтения
+	if(close(fd1[0])<0){
+		printf("Can't close input stream\n");
+		 exit(-1);
+		}
+// Закрытие pipe для записи
+	if (close(fd2[1])<0){
+	  	printf("Can't close output stream\n");
+		exit(-1);
 	}
+	}
+	return 0;
+}
+
+
 
