eabf774e95849906fb8884619f3b65fede44a1c1
Sarana <sarana09@mail.ru>
2024-05-14 13:39:28 +0700

    добавили 2 программы для иллюстрации работы семафоров

--- sems1.c
+++ sems1.c
@@ -4,21 +4,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(){
-	int semid;
-	char pathname[] = "forftok.ipc";
-	key_t key;
-	struct sembuf mybuf;
+	int semid;// идентификатор айписи для массива симафора
+	char pathname[] = "forftok.ipc";//Файл, используемыйдля генерации шзс - ключа
+	key_t key;// айписи ключ
+	struct sembuf mybuf;//Cтруктура для задания операции над семафорам
+ 	//Генерация ключа айписи
 	if((key = ftok(pathname, 0)) < 0){
 		printf("Не удалось сгенерировать ipc-ключ\n");
 		exit(-1);
 	}
-	if ((semid = semget (key, 1, 0666 | IPC_CREAT)) <0){
+	//Получение доступа к м ассиву семафоров по айписи ключу
+	if ((semid = semget(key, 1, 0666 | IPC_CREAT)) <0){
 		printf("Не удалось получить доступ к масству семафоров\n");
 		exit(-1);
 	}
-	mybuf.sem_op = 1;
+	mybuf.sem_op = -1;
 	mybuf.sem_flg = 0;
 	mybuf.sem_num = 0;
+	//Выполняем операции
 	if(semop(semid, &mybuf, 1) <0) {
 		printf("Не удалось выполнить операцию над семафором 0\n");
 		exit(-1);
