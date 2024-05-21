94d1abdd2b2cdb860950bc0bb4749da67e1e2774
Sarana <sarana09@mail.ru>
2024-05-21 13:34:19 +0700

    Добавлены в систему справочника описание циклов

--- Help.java
+++ Help.java
@@ -4,6 +4,8 @@ class Help {
 
 		char choice, ignore;
 
+		for (;;) {
+
 		do {
 			System.out.println("Справочная система: ");
 			System.out.println(" 1. if");
@@ -11,12 +13,15 @@ class Help {
 			System.out.println(" 3. for");
                         System.out.println(" 4. while");
 			System.out.println(" 5. do-while");
+			System.out.println("q -  выход из программы");
 			System.out.println("Bыберите подраздел: ");
 			choice = (char) System.in.read();
 			do { 
 				ignore =(char) System.in.read();
 			} while (ignore !='\n');
-		} while (choice < '1'| choice > '5');
+		} while (choice < '1'| choice > '5'& choice !='q');
+
+		if(choice == 'q') break;
 
 		System.out.println("\n");
 
@@ -50,7 +55,8 @@ class Help {
                                 System.out.println("операторы;");
 				System.out.println("} while(условие);");
                                 break;
-	
+		}
+		System.out.println();
 		}
 	}	
 }
