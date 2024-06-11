f0a3c48cd3badafa724e0811320d237c0bbeaccc
sarana09 <167157662+sarana09@users.noreply.github.com>
2024-06-11 13:37:33 +0700

    Update PolymorphismExample.java

--- PolymorphismExample.java
+++ PolymorphismExample.java
@@ -1,81 +1,156 @@
 // Абстрактный класс Recipe
 abstract class Recipe {
-// Абстрактный метод для вычисления общего количества ингредиентов
-abstract int getTotalIngredients();
+private double quantity; // Количество (граммы, литры и т.д.)
+private String name; // Название рецепта
+
+// Конструктор по умолчанию
+Recipe() {
+quantity = 0.0;
+name = "none";
 }
 
-// Подкласс SaladRecipe, представляющий рецепт салата
-class SaladRecipe extends Recipe {
-private int vegetables;
-private int dressings;
-
-// Конструктор для инициализации количества ингредиентов
-public SaladRecipe(int vegetables, int dressings) {
-this.vegetables = vegetables;
-this.dressings = dressings;
+// Параметризированный конструктор
+Recipe(double qty, String n) {
+quantity = qty;
+name = n;
 }
 
-// Реализация метода getTotalIngredients для салата
+// Конструктор с одинаковыми значениями переменных экземпляра
+Recipe(double qty) {
+quantity = qty;
+name = "none";
+}
+
+// Конструктор для создания одного объекта на основе другого
+Recipe(Recipe ob) {
+quantity = ob.quantity;
+name = ob.name;
+}
+
+// Методы доступа к переменным quantity и name
+double getQuantity() {
+return quantity;
+}
+
+String getName() {
+return name;
+}
+
+void setQuantity(double qty) {
+quantity = qty;
+}
+
+void setName(String n) {
+name = n;
+}
+
+// Абстрактный метод для вычисления необходимых ингредиентов
+abstract void calculateIngredients(int people);
+}
+
+// Подкласс Salad, представляющий салат
+class Salad extends Recipe {
+private String type; // Тип салата
+
+// Конструктор по умолчанию
+Salad() {
+super();
+type = "none";
+}
+
+// Конструктор класса Salad
+Salad(String t, double qty) {
+super(qty, "Салат");
+type = t;
+}
+
+// Конструктор с одним аргументом для создания салата
+Salad(double qty) {
+super(qty);
+type = "обычный";
+}
+
+// Конструктор для создания одного объекта на основе другого
+Salad(Salad ob) {
+super(ob);
+type = ob.type;
+}
+
+// Реализация метода calculateIngredients для салата
 @Override
-int getTotalIngredients() {
-return vegetables + dressings;
+void calculateIngredients(int people) {
+double lettucePerPerson = 50.5; // 50.5 грамм салата на человека
+double tomatoPerPerson = 30.3; // 30.3 грамм помидоров на человека
+double cucumberPerPerson = 20.2; // 20.2 грамм огурцов на человека
+
+double totalLettuce = people * lettucePerPerson;
+double totalTomato = people * tomatoPerPerson;
+double totalCucumber = people * cucumberPerPerson;
+
+System.out.println("Для приготовления " + type + " салата на " + people + " человек нужно:");
+System.out.println(totalLettuce + " грамм салата");
+System.out.println(totalTomato + " грамм помидоров");
+System.out.println(totalCucumber + " грамм огурцов");
 }
 }
 
-// Подкласс SoupRecipe, представляющий рецепт супа
-class SoupRecipe extends Recipe {
-private int vegetables;
-private int spices;
-private int water;
+// Подкласс Soup, представляющий суп
+class Soup extends Recipe {
+private String soupType; // Тип супа
 
-// Конструктор для инициализации количества ингредиентов
-public SoupRecipe(int vegetables, int spices, int water) {
-this.vegetables = vegetables;
-this.spices = spices;
-this.water = water;
+// Конструктор по умолчанию
+Soup() {
+super();
+soupType = "none";
 }
 
-// Реализация метода getTotalIngredients для супа
+// Конструктор класса Soup
+Soup(String s, double qty) {
+super(qty, "Суп");
+soupType = s;
+}
+
+// Конструктор с одним аргументом для создания супа
+Soup(double qty) {
+super(qty);
+soupType = "обычный";
+}
+
+// Конструктор для создания одного объекта на основе другого
+Soup(Soup ob) {
+super(ob);
+soupType = ob.soupType;
+}
+
+// Реализация метода calculateIngredients для супа
 @Override
-int getTotalIngredients() {
-return vegetables + spices + water;
-}
-}
+void calculateIngredients(int people) {
+double potatoPerPerson = 100.0; // 100 грамм картофеля на человека
+double carrotPerPerson = 50.5; // 50.5 грамм моркови на человека
+double meatPerPerson = 150.75; // 150.75 грамм мяса на человека
 
-// Подкласс DessertRecipe, представляющий рецепт десерта
-class DessertRecipe extends Recipe {
-private int flour;
-private int sugar;
-private int eggs;
+double totalPotato = people * potatoPerPerson;
+double totalCarrot = people * carrotPerPerson;
+double totalMeat = people * meatPerPerson;
 
-// Конструктор для инициализации количества ингредиентов
-public DessertRecipe(int flour, int sugar, int eggs) {
-this.flour = flour;
-this.sugar = sugar;
-this.eggs = eggs;
-}
-
-// Реализация метода getTotalIngredients для десерта
-@Override
-int getTotalIngredients() {
-return flour + sugar + eggs;
+System.out.println("Для приготовления супа '" + soupType + "' на " + people + " человек нужно:");
+System.out.println(totalPotato + " грамм картофеля");
+System.out.println(totalCarrot + " грамм моркови");
+System.out.println(totalMeat + " грамм мяса");
 }
 }
 
 // Главный класс для тестирования полиморфизма
 public class PolymorphismExample {
 public static void main(String[] args) {
-// Создание массива рецептов
-Recipe[] recipes = new Recipe[3];
+int numberOfPeople = 10; // Количество человек
 
-// Инициализация массива различными рецептами
-recipes[0] = new SaladRecipe(5, 2);
-recipes[1] = new SoupRecipe(4, 3, 1);
-recipes[2] = new DessertRecipe(2, 3, 4);
+// Создание объектов салата и супа
+Recipe salad = new Salad("Цезарь", 1.5);
+Recipe soup = new Soup("Борщ", 2.0);
 
-// Проход по массиву и вычисление общего количества ингредиентов для каждого рецепта
-for (Recipe recipe : recipes) {
-System.out.println("Общее количество ингредиентов: " + recipe.getTotalIngredients());
-}
+// Вычисление и вывод необходимого количества ингредиентов для салата и супа
+salad.calculateIngredients(numberOfPeople);
+soup.calculateIngredients(numberOfPeople);
 }
 }
